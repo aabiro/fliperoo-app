a44919eef9eeeeb8476f0801b9cdf028a42e9f28
aabiro <aabiro@users.noreply.github.com>
2017-11-18 10:32:48 -0500

    new commit

--- fliperoo-app/src/app/app.module.ts
+++ fliperoo-app/src/app/app.module.ts
@@ -1,16 +1,26 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { ImageUploadModule } from "angular2-image-upload";
 
 
 import { AppComponent } from './app.component';
+import { UploadComponent } from './upload/upload.component';
+import { CompareImageComponent } from './compare-image/compare-image.component';
+import { HeaderComponent } from './header/header.component';
+import { ImageResultComponent } from './image-result/image-result.component';
 
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    UploadComponent,
+    CompareImageComponent,
+    HeaderComponent,
+    ImageResultComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    ImageUploadModule.forRoot(),
   ],
   providers: [],
   bootstrap: [AppComponent]
---
+++ fliperoo-app/src/app/compare-image/compare-image.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-compare-image',
+  templateUrl: './compare-image.component.html',
+  styleUrls: ['./compare-image.component.css']
+})
+export class CompareImageComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
